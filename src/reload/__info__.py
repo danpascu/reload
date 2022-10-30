f1b0fa58b5795dc40d3d824b6c0c1726c196b08a
Dan Pascu <dan@aethereal.link>
2022-10-30 16:12:07 +0000

    Added core project and packaging files

---
+++ src/reload/__info__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2020-present Dan Pascu <dan@aethereal.link>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+__version__ = '0.1.0'
+
+__license__ = 'AGPLv3+'
+__webpage__ = 'https://github.com/danpascu/reload'
+
+__author__ = 'Dan Pascu'
+__copyright__ = f'Copyright 2020-present {__author__}'
---
+++ src/reload/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2020-present Dan Pascu <dan@aethereal.link>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+"""RELOAD (REsource LOcation And Discovery) implementation [RFC 6940]"""
+
+from .__info__ import __version__, __license__, __webpage__, __author__, __copyright__
