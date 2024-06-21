6f83e1e17bec11c06822b4debefd1cff022e19c8
Dan Pascu <dan@aethereal.link>
2024-06-21 17:02:59 +0000

    Added comparison and hashing methods to RelaxNGValidator

--- src/reload/configuration/xml/__init__.py
+++ src/reload/configuration/xml/__init__.py
@@ -84,6 +84,14 @@ class RelaxNGValidator:
         self.schema_path = self.schema_directory / schema_file
         self.schema = etree.RelaxNG(file=self.schema_path)
 
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, RelaxNGValidator):
+            return self.schema_path == other.schema_path
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash(self.schema_path)
+
     def validate(self, element: ETreeElement) -> bool:
         return self.schema.validate(element)
 
