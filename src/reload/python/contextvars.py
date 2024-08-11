e06f482bc4cb078331f9052fe0a4e05bd153b8a0
Dan Pascu <dan@aethereal.link>
2024-08-11 10:04:30 +0000

    Made ContextSpec attributes private

--- src/reload/python/contextvars.py
+++ src/reload/python/contextvars.py
@@ -11,15 +11,15 @@ __all__ = 'ContextSpec', 'run_in_context', 'setup_context'
 
 
 class ContextSpec[T]:
-    context_vars: Mapping[ContextVar[T], T]
-    reset_tokens: Mapping[ContextVar[T], Token[T]]
+    _context_vars: Mapping[ContextVar[T], T]
+    _reset_tokens: Mapping[ContextVar[T], Token[T]]
 
     def __init__(self, context_vars: Mapping[ContextVar[T], T]) -> None:
-        self.context_vars = context_vars
-        self.reset_tokens = {}
+        self._context_vars = context_vars
+        self._reset_tokens = {}
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__qualname__}: {', '.join(f'{var.name}={value!r}' for var, value in self.context_vars.items())}'
+        return f'{self.__class__.__qualname__}: {', '.join(f'{var.name}={value!r}' for var, value in self._context_vars.items())}'
 
     def __enter__(self) -> Self:
         self.setup()
@@ -29,15 +29,15 @@ class ContextSpec[T]:
         self.reset()
 
     def setup(self) -> None:
-        reset_tokens = {var: var.set(value) for var, value in self.context_vars.items()}
-        if not self.reset_tokens:
-            self.reset_tokens = reset_tokens
+        reset_tokens = {var: var.set(value) for var, value in self._context_vars.items()}
+        if not self._reset_tokens:
+            self._reset_tokens = reset_tokens
 
     def reset(self) -> None:
-        if self.reset_tokens:
-            for var, token in self.reset_tokens.items():
+        if self._reset_tokens:
+            for var, token in self._reset_tokens.items():
                 var.reset(token)
-            self.reset_tokens = {}
+            self._reset_tokens = {}
 
 
 setup_context = ContextSpec
