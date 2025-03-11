72d5b99a572549f32008447a95085f433d0b2c35
Dan Pascu <dan@aethereal.link>
2025-03-11 19:47:10 +0000

    Refactored reprproxy to improve recursive call performance

--- src/reload/python/__init__.py
+++ src/reload/python/__init__.py
@@ -9,6 +9,38 @@ from typing import TypeAliasType
 __all__ = 'reprproxy',  # noqa: COM818
 
 
+def better_repr(value: object) -> str:
+    """
+    Provide better representation for certain types.
+
+    The representation will mimic their appearance in the code,
+    which can be useful to make error messages more readable.
+
+    This applies to type aliases, generic aliases, union types,
+    classes and Enum members. Everything else gets their normal
+    representation.
+    """
+
+    match value:
+        case TypeAliasType():
+            return better_repr(value.__value__)
+        case GenericAlias():
+            match value.__origin__:
+                case TypeAliasType():
+                    result = better_repr(value.__origin__.__value__[*value.__args__])
+                case _:
+                    result = f'{value.__origin__.__qualname__}[{', '.join(better_repr(_value) for _value in value.__args__)}]'
+            return result
+        case UnionType():
+            return ' | '.join('None' if _type is NoneType else better_repr(_type) for _type in value.__args__)
+        case Enum():  # this also covers Flag which is a subclass of Enum
+            return f'{value.__class__.__qualname__}.{value.name}'
+        case type():
+            return value.__qualname__
+        case _:
+            return '...' if value is Ellipsis else repr(value)
+
+
 class reprproxy:  # noqa: N801
     """
     A proxy to provide better representation for certain types.
@@ -25,23 +57,6 @@ class reprproxy:  # noqa: N801
         self.value = value
 
     def __repr__(self) -> str:
-        match self.value:
-            case TypeAliasType() as value:
-                return reprproxy(value.__value__).__repr__()
-            case GenericAlias() as value:
-                match value.__origin__:
-                    case TypeAliasType():
-                        result = reprproxy(value.__origin__.__value__[*value.__args__]).__repr__()
-                    case _:
-                        result = f'{value.__origin__.__qualname__}[{', '.join(reprproxy(_value).__repr__() for _value in value.__args__)}]'
-                return result
-            case UnionType() as value:
-                return ' | '.join('None' if _type is NoneType else reprproxy(_type).__repr__() for _type in value.__args__)
-            case Enum() as value:  # this also covers Flag which is a subclass of Enum
-                return f'{value.__class__.__qualname__}.{value.name}'
-            case type() as value:
-                return value.__qualname__
-            case value:
-                return '...' if value is Ellipsis else repr(value)
+        return better_repr(self.value)
 
     __str__ = __repr__
