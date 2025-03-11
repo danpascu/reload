48d4fc586b5a75f1c22f2cde22b6612b79906af5
Dan Pascu <dan@aethereal.link>
2025-03-11 19:16:51 +0000

    Added support for type aliases and generic aliases for reprproxy

--- src/reload/python/__init__.py
+++ src/reload/python/__init__.py
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from enum import Enum
-from types import NoneType, UnionType
+from types import GenericAlias, NoneType, UnionType
+from typing import TypeAliasType
 
 __all__ = 'reprproxy',  # noqa: COM818
 
@@ -16,13 +17,22 @@ class reprproxy:  # noqa: N801
 
     def __repr__(self) -> str:
         match self.value:
+            case TypeAliasType() as value:
+                return reprproxy(value.__value__).__repr__()
+            case GenericAlias() as value:
+                match value.__origin__:
+                    case TypeAliasType():
+                        result = reprproxy(value.__origin__.__value__[*value.__args__]).__repr__()
+                    case _:
+                        result = f'{value.__origin__.__qualname__}[{', '.join(reprproxy(_value).__repr__() for _value in value.__args__)}]'
+                return result
+            case UnionType() as value:
+                return ' | '.join('None' if _type is NoneType else reprproxy(_type).__repr__() for _type in value.__args__)
             case Enum() as value:  # this also covers Flag which is a subclass of Enum
                 return f'{value.__class__.__qualname__}.{value.name}'
-            case UnionType() as value:
-                return ' | '.join('None' if _type is NoneType else _type.__qualname__ for _type in value.__args__)
             case type() as value:
                 return value.__qualname__
             case value:
-                return repr(value)
+                return '...' if value is Ellipsis else repr(value)
 
     __str__ = __repr__
