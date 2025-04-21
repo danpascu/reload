a526ae9e6388848de70a19b68c2b23525b935801
Dan Pascu <dan@aethereal.link>
2025-04-21 18:47:21 +0000

    Expose some inner module content at the toplevel of the trust package

--- src/reload/trust/__init__.py
+++ src/reload/trust/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2024-present Dan Pascu <dan@aethereal.link>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .private import KeyType, load_private_key, save_private_key
+from .x509 import CA, Subject, load_certificate, save_certificate
+
+__all__ = 'CA', 'KeyType', 'Subject', 'load_certificate', 'load_private_key', 'save_certificate', 'save_private_key'
